83b7116e040a0ee63309565fa2251f466e188d42
Miguel Silva <miguel.silva0110@gmail.com>
2022-02-25 12:05:42 +0000

    Changes to patrol points
    Removed the renderer from the patrol points:
    - changed from a square to a gizmo sphere

---
+++ FYPprototype/Assets/Scripts/PatrolPointGizmo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolPointGizmo : MonoBehaviour
+{
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 1f);
+
+    }
+}
